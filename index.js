af2f903fc2f00011e150d4cd2335075cb45327fd
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-03-31 16:09:11 +0300

    more solutions

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] === target) return true;
-    }
-  }
-  return false;
-}
-
-console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
 /* 
   Add your pseudocode here
   -loop through the array items
@@ -21,14 +5,66 @@ console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10));
   -if sum = target, return true
   -else, return false
 */
-
-/*
-  Add written explanation of your solution here
+/* 
+  Write the Big O time complexity of your function here
+  runtime: O(n^2)
+  space: O(n)
 */
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const complementNum = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complementNum) return true;
+    }
+  }
+  return false;
+}
+
+//////////////////////////////////////////////////////////////
+
+// 2nd Solution
+// runtime: O(n)
+// space: O(n)
+function hasTargetSum(array, target) {
+  // create an object to keep track of numbers we've already seen
+  const seenNumbers = {};
+  // iterate through each number in the array
+  for (num of array) {
+    // for the current num, identify a complement that adds to the target (comp = target - num)
+    const complementNum = target - num;
+    // check if any key on our object is the complement. if so, return true.
+    if (complementNum in seenNumbers) return true; // this is syntax to check if a key exists in an object
+    // otherwise, add the number (that is not the complement) to the object
+    seenNumbers[num] = true; // here we just giving the numbers a boolean value, you can give it any value
+  }
+  return false;
+}
+//////////////////////////////////////////////////////////////
+
+/* 
+using a set instead of a normal object.
+Set objects are collections of values. You can iterate through the elements of a set in insertion order. 
+A value in the Set may only occur once; it is unique in the Set's collection.
+Time complexity: O(n)
+Space complexity: O(n)
+ */
+
+function hasTargetSum(array, target) {
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log('Expecting: true');
   console.log('=>', hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -41,6 +77,23 @@ if (require.main === module) {
 
   console.log('Expecting: false');
   console.log('=>', hasTargetSum([1, 2, 5], 4));
+
+  console.log('');
+
+  console.log('');
+  // Negative numbers?
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log('');
+  // Multiple pairs?
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log('');
+  // Single numbers?
+  console.log('Expecting: false');
+  console.log('=>', hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
